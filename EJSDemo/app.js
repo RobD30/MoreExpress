220e3c55c697c67832ad40cbdc087500971cb143
RobD30 <rob.dickerson.dev@gmail.com>
2018-05-30 03:20:26 +0000

    Generated love and posts EJS. love has more functionality now

--- EJSDemo/app.js
+++ EJSDemo/app.js
@@ -5,6 +5,21 @@ app.get('/', function(req, res){
     res.render('home.ejs');
 });
 
+app.get('/fallinlovewith/:thing', function(req, res){
+    var thing = req.params.thing;
+    res.render('love.ejs', {thingVar: thing});
+});
+
+app.get('/posts', function(req, res) {
+    var posts = [
+        {title: 'How I beat up Stewey Gryphin', author: 'Susy'},
+        {title: 'My adorable pet bunny', author: 'Rob'},
+        {title: 'Dear god they are everywhere', author: 'Charlie'},
+    ];
+        
+    res.render('posts.ejs', {posts: posts});
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('SERVER IS LISTENING');
 });
